00da2b03d80606b18d4221b7d9a11e8877942364
TopFusion.Dev <top.ramet@gmail.com>
2022-05-25 16:05:06 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const expressWs = require('express-ws')(app);
+const cors = require('cors')
+
+//use services
+app.use(cors({
+    origin: '*'
+}));
+app.use(express.text());
+app.use(express.json());
+
+const port = 80
+let ws = null;
+
+app.use(function (req, res, next) {
+    console.log('middleware');
+    req.testing = 'testing';
+    return next();
+});
+
+app.get('/', function (req, res, next) {
+    console.log('get route server', req.testing);
+    res.send("hello from server")
+    // res.end();
+});
+
+// //webhook
+app.post('/', async (req, res, next) => {
+    console.log('received webhook0', req.body);
+    let receivedData = await req.body
+    receivedData.autoId = String(new Date())
+    console.log('receivedData', receivedData)
+
+    if (ws && receivedData?.autoId) ws.send(JSON.stringify(receivedData));
+     res.send('ok')
+});
+
+app.ws('/', function (socket, req) {
+    ws = socket
+    console.log('[Server] get connected')
+    socket.on('message', function (msg) {
+        console.log('ws:// server message received', msg);
+    });
+    socket.send('Hello world!');
+    // console.log('socket', req.testing);
+    socket.on('close', function close() {
+        // จะทำงานเมื่อปิด Connection ในตัวอย่างคือ ปิด Browser
+        console.log('disconnected');
+    });
+});
+
+
+app.listen(port);
---
+++ src/pine/test1.js
@@ -0,0 +1,24 @@
+{
+    "id": "testtset",
+    "awesomeVersion": "A1.8"
+    "actionOrder": "sell",
+    "forexName": "XAUUSD",
+    "comment": "sell 02",
+    "plotStatus0: "1863.86"",
+    "plotStatus1": "1863.86",
+    "plotStatus2": "null",
+    "plotStatus3": "null",
+    "plotStatus4": "null",
+    "plotStatus5": "null",
+    "positionSize": "-800",
+    "prevMarket": "flat",
+    "market": "short",
+    "strategyDataAll": "{{strategy}}",
+    "strategyEntry": "{{strategy.entry}}",
+    "strategyClose": "{{strategy.close}}",
+    "strategyOrderAlertMessage": "",
+    "strategyExit": "{{strategy.exit}}",
+    "plotShape": "{{plotshape}}",
+    "plotArrow": "{{plotarrow}}",
+    "plotChart”: "{{plotchart}}"
+}
